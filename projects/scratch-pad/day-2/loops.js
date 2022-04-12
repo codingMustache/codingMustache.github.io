904bcce27daae12854f524b9b9e6840fb6e16638
Jorge <jorcar1986@gmail.com>
2022-04-12 15:44:19 -0500

    greeter app done

--- projects/scratch-pad/day-2/loops.js
+++ projects/scratch-pad/day-2/loops.js
@@ -12,10 +12,9 @@
  */
 function printArrayValues(array) {
   // YOUR CODE BELOW HERE //
-  
-  
-  
-  
+  for (var i = 0; i <= array.length; i++){
+    console.log(array[i])
+  }
   // YOUR CODE ABOVE HERE //
 }
 
@@ -25,10 +24,7 @@ function printArrayValues(array) {
  */
 function printArrayValuesInReverse(array) {
   // YOUR CODE BELOW HERE //
-  
-  
-  
-  
+  for (var i = array.length - 1; )
   // YOUR CODE ABOVE HERE //
 }
 
