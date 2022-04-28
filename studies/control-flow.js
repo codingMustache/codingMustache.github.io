c949528af68b015df678f6cb71d3e237f82c4470
Jorge <jorcar1986@gmail.com>
2022-04-27 20:38:07 -0500

    worked on studies

--- studies/control-flow.js
+++ studies/control-flow.js
@@ -10,21 +10,31 @@
 /* 
 
 */
+if(1 < 2) {console.log("This is true")};      //prints => This is true
 
+if(1 > 2) {console.log("This is not true")};  // prints =>
 
 
 // 2. Else-if
 /* 
  
 */
-
+if (1 > 2) {
+    console.log('This will not run');
+} else if (1 < 2) {
+    console.log('This will run if this is true');
+}   // prints => "This will run if this is true"
 
 
 // 3. Else
 /* 
  
 */
-
+if (1 > 2) {
+    console.log('This will not run');
+} else {
+    console.log('This will run');
+}   // prints => "This will run"
 
 // 4. Switch
 /* 
--- studies/loops.js
+++ studies/loops.js
@@ -8,7 +8,11 @@
  
 */
 /////    Be able to loop any number of times, forward counting up to some limit, backward counting down to 0
-
+var i = 0;              // prints => 0
+while(i < 3){           //           1   
+  console.log(i);       //           2
+  i++;                  //           3
+}
 
 
 // 2. For Loops
@@ -16,8 +20,10 @@
 
 */
 ////     Be able to loop any number of times, forward counting up to some limit, backward counting down to 0
-
-
+var nums = "";
+for(let i = 0; i <= 3; i++)
+{text += i + " "; }
+console.log(text) // prints => 1 2 3
 
 // 3. For-In Loops
 /* 
@@ -32,7 +38,12 @@
  
 */
 ////  Loop over an Array, forwards and backwards  
-
+var arr = ['this','is','a','for','loop']
+var text = ""
+for(let i = 0; i < arr.length; i++){
+   text += arr[i] + " ";
+}
+console.log(text) // prints => this is a for loop
 
 
 // 5. Looping Over Object
@@ -40,3 +51,9 @@
  
 */
 ////    Loop over an object
+var obj = {1:'this', 2:'is', 3:'a', 4:'for-in', 5:'loop'}
+var text = ""
+for(var key in obj){
+   text += obj[key] + " ";
+}
+console.log(text) // prints => this is a for-in loop
