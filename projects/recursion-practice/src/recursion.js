aca1d5b681eb596ae9b13db7dd28bfeda187c8fb
Jorge <jorcar1986@gmail.com>
2022-05-02 20:48:39 -0500

    recursion

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -133,7 +133,16 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
-  
+  if (string.length < 2){ 
+    return true
+  };
+  if (string[0] === string[string.length - 1] ) {
+      return palindrome(string.slice(1, string.length - 1) );
+  } else if( string[0] === " " || string[0] === string[0].toUpperCase){
+    return palindrome()
+  }
+
+  return false;
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
