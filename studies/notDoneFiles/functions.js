5559c19f5e25c343f9aa1c06adc6679160c2feb1
Jorge <jorcar1986@gmail.com>
2022-05-15 18:43:04 -0500

    function.js is almost done

--- studies/notDoneFiles/functions.js
+++ studies/notDoneFiles/functions.js
@@ -15,9 +15,6 @@
         funciton downward. With the exception of block/function 
         hoisting within the function. 
         */
-////////    First we must ___? Next we can execute 
-        ///////     (or two other words for executing a
-        ///////     function?) a function by?
 
 
 // 2. Parameters vs Passed Arguments
@@ -48,43 +45,127 @@
         */
                 function named(p){ // Na
                         // The function body
-                 }
+                 },
 
-                 
-// 4. Assigning Functions to a Variable
+// 4. Arrow Function
+        /*
+        a. Is a shorthand of traditional functions
+        b. Should not be used used as methods
+        c. Do not have thier own binding to this, arguements and super
+        d. Can not be used as Constructors 
+        e. Can use implcit return by removing curly braces
+        */
+                var arrow () => {console.log('Arrow function')}
+                arrow() // prints -> Arrow function
+                var arr1 = x => x + 1
+                arr1(1) // prints -> 2 
+        
+// 5. Anonymous Function
+        /*
+        a. A anonymous funciton is a functino with no name associated with it.
+        b. It can not be accessed after its creattion
+        c. If asigned to a variable is the only way to invoke an anonymous 
+                funciton more than once.
+        d. Can have multiple arguements but only one expression. 
+        */
+        (function (){ console.log("I am ANON")})(); // prints -> I am ANON
+                var paraAnonFunc = "anon";
+                var anon = function(y){
+                        console.log(y)
+                }
+                anon(paraAnonFunc) // prints -> anon
+
+        
+// 7. Assigning Functions to a Variable
         /* 
-        a. 
+        a. Functions can be assigned as variables with the var let and const keywords
+        b. When const is used the function name can not be reasigned.
+        c. Invoking a named variable is similar to a named fucntion, calling it and 
+                passing any arguments into the parenthesis.
         */
 /////    How do we assign a function to a variable?
-                var x = "Assigned to a Variable"     
-                const vari = function(v){
-                        console.log(v)
+                var arg = "Asigned variable"
+                var assignedVariable = function (v) {
+                        console.log(v);
                 }
-                vari(x); 
+                assignedVariable(arg); 
                         
         
-// 5. Specify Input/Output Functions
+// 8. Specify Input/Output Functions
         /*  
-        a.
+        a. Reading/writing documentation is key to understanding what kind of inputs
+                a function handles or output it returns.
+        b. Writing good comments that shows the kind of data to be output by the funcitons
+                will lead to a better understanding of how a function handles data
         */
-        ////    Functions can OPTIONALLY take inputs, and
-        ////    OPTIONALLY return a single value. How do 
-        ////    we specify inputs, and how do we specify outputs?
-        
-        
 
 
-
-// 6. Scope
+// 9. Scope
         /* 
-        a.
+        a. Function Scope
+                i. Is the scope within the function
+                ii. Variebles within functions are not effected by outter ones
+                and vice-versa
+        */    
+        function scope(){
+                let msg = "I"           // nothing within the funciton
+                var msg1 = "am"         // can be accesed outside the function
+                const msg2 = "scope"    // except what is returned from the function
+        }
+        console.log(msg)  // prints -> ReferenceError
+        console.log(msg1) // prints -> ReferenceError
+        console.log(msg2) // prints -> ReferenceError
+        
+        let msg = "outer"
+        function scope1(){
+                let msg = "Indside";    // Because of scope the inner variable
+                console.log(msg);       // msg is not effecting the outer one
+        }
+        scope1() // prints -> Inside
+        console.log(msg) // //prints -> outer
+
+       /*
+        b. Block Scope
+                i. Refers to scope within a block of code in loops, as for and while
+                ii. The var key word is not held within these contraints
+                iii. Let and Const keywords are not accessable outside of block scope 
         */
-        ////    Scope: Functions can see and modify variables in 
-        ////    parent or global scopes. The inverse is NOT true.
+       var var4 = "we" 
+       let var5 = "Can"
+       const var6 = "Reach"
+        for(let i = 0; i < 1; i++){
+                console.log(var4, var5, var6) // prints -> weCanReach
+                var var1 = "hello"
+                let  var2 = "Nope"
+                const var3 = "No way"
+        }
+        console.log(vari1) // prints -> hello
+        console.log(var2) // prints-> ReferenceError
+        console.log(var3) // prints-> ReferenceError
 
+        /*
+        c. Lexical Scope
+                i. A function can see and modify a varible in its parent or global
+                scope but not the other way.
+                ii. As nested as a function may bet it will look outward till a 
+                variable definition
+        */
+       function outer(){
+               let planet = "mercury"
+               function inner(){
+                       let planet = "venus"
+                       function blackHole(){
+                                let planet = "earth"
+                                console.log(planet)
+                       }
+                       blackHole()
+               }
+               inner()
+       }
+       outer() // prints -> Earth
+ 
 
-
-// 7. Closure
+// 10. Closure
         /* 
         a.
         */
